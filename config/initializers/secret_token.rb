f2c17c33d464fbaf15b8ee390faf12c054972ad1
Ethan Soutar-Rau <ethan.soutar.rau@gmail.com>
2011-04-18 17:46:17 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cove::Application.config.secret_token = 'd1956523e3eba998d67a8706e62eca9d356b38ef88af933cb6c97b8b62c375d2f2f4d1c5d06e8d974559d9c9577a631eb37c248c2bebd7a8a5af662fcbb746ae'
