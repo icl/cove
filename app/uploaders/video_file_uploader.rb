44c8ac6b572edda5ca1ff62941f7b93fa9d28438
PratikPramanik <ppramaniucsd@gmail.com>
2011-04-25 00:01:47 -0700

    got rid of test crap

--- app/uploaders/video_file_uploader.rb
+++ app/uploaders/video_file_uploader.rb
@@ -1,6 +1,6 @@
 # encoding: utf-8
 
-class AvatarUploader < CarrierWave::Uploader::Base
+class VideoFileUploader < CarrierWave::Uploader::Base
 
   # Include RMagick or ImageScience support:
   # include CarrierWave::RMagick
@@ -13,8 +13,7 @@ class AvatarUploader < CarrierWave::Uploader::Base
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
-    #"uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
-    "uploads/file_dump/"
+    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
