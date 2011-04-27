710af9508d5fa9c5b23c3c6a62a0fe1ba6160913
PratikPramanik <ppramaniucsd@gmail.com>
2011-04-27 15:46:09 -0700

    small

--- db/migrate/20110427211616_add_more_to_videos.rb
+++ db/migrate/20110427211616_add_more_to_videos.rb
@@ -1,14 +1,13 @@
 class AddMoreToVideos < ActiveRecord::Migration
   def self.up
-    add_column :videos, :orig_filename, :string
-    add_column :videos, :source_cam, :string
-    add_column :videos, :project, :string
-    add_column :videos, :created, :datetime
-    add_column :videos, :uploaded, :datetime
-    add_column :videos, :offset, :integer
-    add_column :videos, :location, :string
-    add_column :videos, :orig_res, :string
-    add_column :videos, :cam_type, :string
+    add_column :videos, :orig_filepath, :string #Filename and path as it was from the source ("G:/External/12_31_30/file.mp4")
+    add_column :videos, :source_cam, :string #Camera that was used to create the video ("Balanchine")
+    add_column :videos, :project, :string #Project the video was created for ("Dance Ethnography Project" "Wiisard")
+    add_column :videos, :uploaded, :datetime #when was the file uploaded
+    add_column :videos, :offset, :integer #Known clock deviation from the source Camera's internal clock ("3 seconds")
+    add_column :videos, :location, :string #Location of filming, not coordinates ("Practice Room")
+    add_column :videos, :orig_res, :string #Highest resolution available, the one it was filmed in ("1920x1080, 30fps")
+    add_column :videos, :cam_type, :string #Type of camera used to make video ("Canon Vixia HD 880")
   end
 
   def self.down
