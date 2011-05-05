756a15fa1399f749934dbb85d8fe71f3937da991
adodge <alexdodge@gmail.com>
2011-05-05 14:28:29 -0700

    Getting started hooking up devise.  Probably just broke every cucumber test.

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,11 @@ Cove::Application.routes.draw do
   resources :jobs
   match 'jobs/:id/work' => 'jobs#work'
 
-  devise_for :users
+  devise_for :users, :path => "/", :path_names => {
+	  :sign_in => "login",
+	  :sign_out => "logout",
+	  :sign_up => "register"
+  }
 
   root :to => "home#index"
   # The priority is based upon order of creation:
