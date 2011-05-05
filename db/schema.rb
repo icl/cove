e577370fd32d3a824a39aadde2985a6f61e8c282
adodge <alexdodge@gmail.com>
2011-05-05 16:19:32 -0700

    Adding a default value for user kind

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110502233447) do
+ActiveRecord::Schema.define(:version => 20110505231331) do
 
   create_table "codes", :force => true do |t|
     t.string   "name"
@@ -54,8 +54,8 @@ ActiveRecord::Schema.define(:version => 20110502233447) do
   add_index "rails_admin_histories", ["item", "table", "month", "year"], :name => "index_histories_on_item_and_table_and_month_and_year"
 
   create_table "users", :force => true do |t|
-    t.string   "email",                                 :default => "", :null => false
-    t.string   "encrypted_password",     :limit => 128, :default => "", :null => false
+    t.string   "email",                                 :default => "",     :null => false
+    t.string   "encrypted_password",     :limit => 128, :default => "",     :null => false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
@@ -66,7 +66,7 @@ ActiveRecord::Schema.define(:version => 20110502233447) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "kind"
+    t.string   "kind",                                  :default => "turk"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
