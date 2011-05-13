6df705da3e215ddf44df6cc84898f036ab65cea1
Ryan Weald <ryan@weald.com>
2011-05-12 17:33:48 -0700

    add a timestamp and uuid to all the log data entries

--- app/controllers/loggings_controller.rb
+++ app/controllers/loggings_controller.rb
@@ -1,7 +1,10 @@
 class LoggingsController < ApplicationController
   respond_to :json
   def create
-    Rails.event_log.write(params[:data])
+    uuid = ActiveSupport::SecureRandom.hex(12)
+    timestamp = DateTime.now.to_s
+    log_data = { "timestamp" => timestamp, "_id" => uuid, "data" => params[:data] }
+    Rails.event_log.write(JSON.dump(log_data))
     respond_with do |format|
       format.json { render :json => {:status => "success"} }
     end
