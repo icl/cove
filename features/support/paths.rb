0783799efb018ace2a8b9c08e395ef69e95e20e2
Aaron <aaron@ubuntu.ubuntu-domain>
2011-05-13 22:50:46 -0700

    Added fortuity's devise steps for cucumber. Created cucumber feature for training module.

--- features/support/paths.rb
+++ features/support/paths.rb
@@ -11,14 +11,17 @@ module NavigationHelpers
     when /the home\s?page/
       '/'
 
+    when /the sign up page/
+      '/users/sign_up'
+
+    when /the sign in page/
+      '/users/sign_in'
+
     # Add more mappings here.
     # Here is an example that pulls values out of the Regexp:
     #
     #   when /^(.*)'s profile page$/i
     #     user_profile_path(User.find_by_login($1))
-
-    when /the work page/
-      "/jobs/#{@job.id}/work"
 
     else
       begin
