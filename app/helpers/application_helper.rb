d4291fbcc39091ad04c3f334c8d98f150f9abd57
Ethan Soutar-Rau <ethan.soutar.rau@gmail.com>
2011-06-08 16:26:07 -0700

    modify to work with video server

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,17 @@
 module ApplicationHelper
+  
+  def  video_service_url file_name
+
+  token = "#{CoveConfig.video_prefix}#{get_video_token(file_name)}/#{file_name}"
+  
+  
+  
+  end
+  
+  def get_video_token file_name
+    puts "#{CoveConfig.token_prefix}?video=#{file_name}"
+    JSON.parse(RestClient.get "#{CoveConfig.token_prefix}?video=#{file_name}" )['token']
+    
+  end
+
 end
