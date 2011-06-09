4a0ceb89fe6a7f8c048bb8a206a4e7fa173349e3
Ryan Weald <ryan@weald.com>
2011-06-09 15:50:29 -0700

    fix the search_results view so that it actually renders the results

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -3,15 +3,16 @@ class SearchController < ApplicationController
     #@query = params[:video] ? params[:video][:query] : ""
     @query = params[:query] ? params[:query][0] : ""
     if @query.blank?
-      @results = Video.all
+      @results = Video.where("")
     else
       @results = Video.search(@query.downcase)
-      unless params[:video].blank?
-        params[:video].each do |key, value| 
-          @results = @results.where(key => value) unless value.blank?
-        end
+    end
+    unless params[:video].blank?
+      params[:video].each do |key, value| 
+        @results = @results.where(key => value) unless value.blank?
       end
     end
+    
     respond_to do |format|
       format.html do
         if request.xhr?
